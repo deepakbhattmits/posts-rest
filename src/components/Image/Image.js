d5f793d1b3f443e5b6fd469461ebdd5affe7aa96
deepak bhatt <deepakbhattmits@gmail.com>
2020-12-06 00:02:32 +0530

    Update Image.js

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,16 +1,18 @@
+/** @format */
+
 import React from 'react';
 
 import './Image.css';
 
-const image = props => (
-  <div
-    className="image"
-    style={{
-      backgroundImage: `url('${props.imageUrl}')`,
-      backgroundSize: props.contain ? 'contain' : 'cover',
-      backgroundPosition: props.left ? 'left' : 'center'
-    }}
-  />
+const image = ({ imageUrl, contain, left }) => (
+	<div
+		className='image'
+		style={{
+			backgroundImage: `url(${imageUrl})`,
+			backgroundSize: contain ? 'contain' : 'cover',
+			backgroundPosition: left ? 'left' : 'center',
+		}}
+	/>
 );
 
 export default image;
